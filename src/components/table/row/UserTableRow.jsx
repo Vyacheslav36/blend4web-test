979938fed2e2b13cd4f6975d399fd0413588e3c6
vdyakov <vdyakov@angelsit.ru>
2020-08-06 01:11:26 +0300

    feat: added methods for drag events

--- src/components/table/row/UserTableRow.jsx
+++ src/components/table/row/UserTableRow.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import PropTypes from 'prop-types';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import ViewButton from '../../buttons/ViewButton';
 import EditButton from '../../buttons/EditButton';
 import DeleteButton from '../../buttons/DeleteButton';
@@ -33,25 +34,76 @@ const ActionsGroup = ({ handleRemoveItem, handleEditRow, handleViewRow }) => {
   );
 };
 
+const useStyles = makeStyles((theme) => ({
+  hidden: {
+    display: 'none',
+  },
+  hover: {
+    borderTop: '3px solid #333333',
+  },
+}));
+
 const UserTableRow = ({
   row, handleRemoveItem, handleEditRow, handleViewRow
-}) => (
-  <TableRow
-    hover
-    data-id={row.id}
-  >
-    <TableCell align="left">{row.name}</TableCell>
-    <TableCell align="left">{row.type}</TableCell>
-    <TableCell align="left">{row.color}</TableCell>
-    <TableCell align="right">
-      <ActionsGroup
-        handleRemoveItem={handleRemoveItem}
-        handleEditRow={handleEditRow}
-        handleViewRow={handleViewRow}
-      />
-    </TableCell>
-  </TableRow>
-);
+}) => {
+  const classes = useStyles();
+
+  const dragStart = (e) => {
+    const el = e.currentTarget;
+    e.dataTransfer.setData('Id', el.dataset.id);
+    setTimeout(() => el.classList.add(classes.hidden), 0);
+  };
+
+  const dragEnd = (e) => {
+    e.currentTarget.classList.remove(classes.hidden);
+  };
+
+  const dragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const dragEnter = (e) => {
+    e.currentTarget.classList.add(classes.hover);
+  };
+
+  const dragLeave = (e) => {
+    e.currentTarget.classList.remove(classes.hover);
+  };
+
+  const dragDrop = (e) => {
+    const elementId = e.dataTransfer.getData('Id');
+    const draggableElement = document.querySelector(`[data-id="${elementId}"]`);
+    if (draggableElement) {
+      e.currentTarget.before(draggableElement);
+    }
+    e.currentTarget.classList.remove(classes.hover);
+  };
+
+  return (
+    <TableRow
+      hover
+      data-id={row.id}
+      draggable
+      onDragStart={dragStart}
+      onDragOver={dragOver}
+      onDragEnter={dragEnter}
+      onDragLeave={dragLeave}
+      onDragEnd={dragEnd}
+      onDrop={dragDrop}
+    >
+      <TableCell align="left">{row.name}</TableCell>
+      <TableCell align="left">{row.type}</TableCell>
+      <TableCell align="left">{row.color}</TableCell>
+      <TableCell align="right">
+        <ActionsGroup
+          handleRemoveItem={handleRemoveItem}
+          handleEditRow={handleEditRow}
+          handleViewRow={handleViewRow}
+        />
+      </TableCell>
+    </TableRow>
+  );
+};
 
 UserTableRow.propTypes = {
   row: PropTypes.object.isRequired,
